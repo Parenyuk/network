68267e3e3b8ad1ca008f536802e7339e64b5d64b
Victor <victorparenyuk@gmail.com>
2020-03-16 10:48:05 +0200

    twenty lesson task

--- src/components/LeftNavbar/LeftNavbar.js
+++ src/components/LeftNavbar/LeftNavbar.js
@@ -1,14 +1,15 @@
 import React from "react";
 import s from './LeftNavbar.module.css'
+import {NavLink} from "react-router-dom";
 
 const LeftNavbar = () => {
     return (
         <nav className={s.lnav} >
-            <div><a href="/profile">Profile</a></div>
-            <div><a href="/messages">Messages</a></div>
-            <div><a href="/news">News</a></div>
-            <div><a href="/audio">Audio</a></div>
-            <div><a href="/settings">Settings</a></div>
+            <div><NavLink to="/profile" activeClassName={s.activeLink} >Profile</NavLink>  </div>
+            <div><NavLink to="/messages" activeClassName={s.activeLink}>Messages</NavLink></div>
+            <div><NavLink to="/news" activeClassName={s.activeLink}>News</NavLink></div>
+            <div><NavLink to="/audio" activeClassName={s.activeLink}>Audio</NavLink></div>
+            <div><NavLink to="/settings" activeClassName={s.activeLink}>Settings</NavLink></div>
         </nav>
     )
 }
