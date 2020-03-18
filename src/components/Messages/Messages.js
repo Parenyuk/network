a7ee7ee9cde01bc54ff0ac7000b0ac65548f2769
Parenyuk <48602407+Parenyuk@users.noreply.github.com>
2020-03-18 22:14:22 +0200

    twenty-fifth task do not work

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -2,14 +2,11 @@ import React from "react";
 import s from './Messages.module.css';
 import {NavLink} from "react-router-dom";
 
-
-
 const Message = (props) => {
 
     return (
-        <div className={s.message}>
-            <img src='https://whatsism.com/uploads/posts/2018-07/1530544023_n6fgwzftnvg.jpg' />
-            <div className={s.head}>
+        <div className={s.messages}>
+            <div className={s.messagesItem}>
                 <div className={s.userName}><NavLink to={`/message/${props.id}`}>{props.name}</NavLink> </div>
                 <div className={s.text}>{props.message}</div>
             </div>
@@ -18,26 +15,41 @@ const Message = (props) => {
 }
 
 
-const Messages = () => {
-    const messagesData = [
-        {id: 1, name: 'Victor', message:`Hi`},
-        {id: 2, name: 'Vasia Pypkin', message: `kyky`},
-        {id: 3, name: 'Zinedin Zidan', message: `Hello`},
-        {id: 4, name: 'Ronaldo', message: `Hello`},
-        {id: 5, name: 'Vasia Pypkin', message: `Hello`},
+const Messages = (props) => {
+
+    let dialogsData = [
+        {id: 1, name: 'Victor'},
+        {id: 2, name: 'Vasia Pypkin'},
+        {id: 3, name: 'Zinedin Zidan'},
+        {id: 4, name: 'Ronaldo'},
+        {id: 5, name: 'Vasia Pypkin'},
+        {id: 6, name: 'Messi'}
     ]
 
+    let messagesData = [
+        {id: 1,  message:`Hi`},
+        {id: 2,  message: `kyky`},
+        {id: 3,  message: `Hello`},
+        {id: 4,  message: `Hello`},
+        {id: 5,  message: `Hello`},
+        {id: 6,  message: `The ejkgje`},
+    ]
+    let dialogsDataElement = dialogsData.map(d => <Message id={d.id} name={d.name}  />)
+    let messagesDataElement = messagesData.map(m => <Message id={m.id} message={m.message}/>)
+
     return (
-        <div className={s.messages}>
-            <div className={s.messagesItem}>
-                <Message id={messagesData[0].id} name = {messagesData[0].name} message={messagesData[0].message} />
-                <Message id={messagesData[1].id} name = {messagesData[1].name} message={messagesData[1].message}/>
-                <Message id={messagesData[2].id} name = {messagesData[2].name} message={messagesData[2].message} />
-                <Message id={messagesData[3].id} name = {messagesData[3].name} message={messagesData[3].message}/>
-                <Message id={messagesData[4].id} name = {messagesData[4].name} message={messagesData[4].message}/>
+        <div className={s.message}>
+            <img src='https://whatsism.com/uploads/posts/2018-07/1530544023_n6fgwzftnvg.jpg' />
+            <div className={s.head}>
+                <div>{dialogsDataElement}</div>
+                <div>{messagesDataElement}</div>
+
+
             </div>
         </div>
     )
 }
+
+
 
 export default Messages;
