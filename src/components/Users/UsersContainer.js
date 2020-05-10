4289d035bbc34319a50a1ebc65dc5996a459843c
Victor <ParenVic@i.ua>
2020-05-10 22:29:36 +0300

    fifty five task full vorking variant

--- src/components/Users/UsersContainer.js
+++ src/components/Users/UsersContainer.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Users from "./Users";
 import {connect} from "react-redux";
-import {followAC, setUsersAC, unfollowAC} from "../../redux/UsersReducer";
+import {followAC, setUsersAC, unfollowAC, setCurrentPageAC, setUsersTotalCountAC} from "../../redux/UsersReducer";
 
 
 let mapStateToProps = (state) => {
     return {
-        users: state.usersPage.users
+        users: state.usersPage.users,
+        pageSize: state.usersPage.pageSize,
+        totalUsersCount: state.usersPage.totalUsersCount,
+        currentPage: state.usersPage.currentPage
     }
 }
 
@@ -20,9 +23,14 @@ let mapDispatchToProps = (dispatch) => {
         },
         setUsers: (users) => {
             dispatch(setUsersAC(users));
+        },
+        setCurrentPage: (pageNumber) => {
+            dispatch(setCurrentPageAC(pageNumber))
+        },
+        setTotalUsersCount: (totalCount) => {
+            dispatch(setUsersTotalCountAC(totalCount))
         }
     }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Users);
-
