df38838b37c09a09eceaf115d83c333b780aafe2
Victor <ParenVic@i.ua>
2020-08-02 14:29:07 +0300

    Updates

---
+++ precache-manifest.87b97863f3ebc5f57e0968fccf8d930f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e0df15d6a89211d91f0383b114a01bc",
+    "url": "/network/index.html"
+  },
+  {
+    "revision": "f2e7d3c32e014d97d2be",
+    "url": "/network/static/css/3.3e295f60.chunk.css"
+  },
+  {
+    "revision": "6fe5320e2dad64cd24fe",
+    "url": "/network/static/css/4.3969e79f.chunk.css"
+  },
+  {
+    "revision": "d855ca10b31aba856343",
+    "url": "/network/static/css/main.130a229f.chunk.css"
+  },
+  {
+    "revision": "603541120ceaa4e62ecb",
+    "url": "/network/static/js/2.6411a431.chunk.js"
+  },
+  {
+    "revision": "7410b1c846886bcecbd03609e529eb8d",
+    "url": "/network/static/js/2.6411a431.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2e7d3c32e014d97d2be",
+    "url": "/network/static/js/3.a13f27b8.chunk.js"
+  },
+  {
+    "revision": "6fe5320e2dad64cd24fe",
+    "url": "/network/static/js/4.2714df7e.chunk.js"
+  },
+  {
+    "revision": "d855ca10b31aba856343",
+    "url": "/network/static/js/main.4e5373bd.chunk.js"
+  },
+  {
+    "revision": "17d00adc95cd28a84558",
+    "url": "/network/static/js/runtime-main.38803649.js"
+  },
+  {
+    "revision": "3995fd5a4ffa943202e080317a7aba2c",
+    "url": "/network/static/media/Spinner-1s-200px.3995fd5a.svg"
+  }
+]);
